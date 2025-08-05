21a76d64c0db78b64881a83c12e8beb5274081d8
Olivier Mac 2024 <olgil12@ulaval.ca>
2025-08-05 16:58:22 -0400

    transform function for ndimage.rotate + deleted galight

--- galight_modif/__init__.py
+++
@@ -1,4 +0,0 @@
-__author__ = 'Xuheng Ding'
-__email__ = 'xuheng.ding@ipmu.jp'
-__version__ = '0.2.0'
-__credits__ = 'IPMU, UCLA'
---
+++ test.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.ndimage import rotate
+import os
+from matplotlib.image import imread
+
+
+lights_folder = r"lights/"
+darks_folder = r"darks/"
+
+lights_paths = [f for f in os.listdir(lights_folder) if os.path.isfile(os.path.join(lights_folder, f))]
+darks_paths = [f for f in os.listdir(darks_folder) if os.path.isfile(os.path.join(darks_folder, f))]
+lights = [np.array(imread(f"{lights_folder}{lights_paths[i]}")) for i in range(len(lights_paths))]
+darks = [np.array(imread(f"{darks_folder}{darks_paths[i]}")) for i in range(len(darks_paths))]
+
+img_unrotated = lights[0]
+ANGLE = 300-360
+img_rotated = rotate(input=img_unrotated, angle=ANGLE, reshape=True)
+
+def transform_fn(x:float|np.ndarray, y:float|np.ndarray, theta:float, shape:tuple) -> tuple :
+    height, width = shape
+    theta = theta - np.floor(theta / (2*np.pi))*2*np.pi # Make it so theta is between 0 and 2*np.pi
+    if theta <= np.pi/2:
+        x_ = x*np.cos(theta)+y*np.sin(theta)
+        y_ = y*np.cos(theta)-x*np.sin(theta)+width*np.sin(theta)
+    elif (theta > np.pi/2) and (theta <= np.pi):
+        x_ = x*np.cos(theta)+y*np.sin(theta)+width*np.sin(theta-np.pi/2)
+        y_ = y*np.cos(theta)-x*np.sin(theta)+width*np.sin(theta)+height*np.sin(theta-np.pi/2)
+    elif (theta > np.pi) and (theta <= 3*np.pi/2):
+        x_ = x*np.cos(theta)+y*np.sin(theta)+width*np.cos(theta-np.pi)+height*np.sin(theta-np.pi)
+        y_ = y*np.cos(theta)-x*np.sin(theta)+height*np.cos(theta-np.pi)
+    else:
+        x_ = x*np.cos(theta)+y*np.sin(theta)+height*np.cos(theta-3*np.pi/2)
+        y_ = y*np.cos(theta)-x*np.sin(theta)
+    return (x_, y_)
+
+
+ax1 = plt.subplot(121)
+ax2 = plt.subplot(122)#, sharex=ax1, sharey=ax1)
+ax1.imshow(img_unrotated, origin="lower")
+ax2.imshow(img_rotated, origin="lower")
+pos = (500,400)
+ax1.plot(pos[0],pos[1], marker="o", color="red", ls="None")
+pos_ = transform_fn(*pos, theta=ANGLE/180*np.pi, shape=img_unrotated.shape[:2])
+ax2.plot(pos_[0],pos_[1], marker="o", color="red", ls="None")
+plt.show()
